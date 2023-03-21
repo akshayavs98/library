d52a4b6fe27e549e8f1cde6ad5344e4416ab73a8
Akshaya <akshayavattaparambil@gmail.com>
2023-03-21 16:07:21 +0100

    initial landing page and popup developer

---
+++ Book.js
@@ -0,0 +1,11 @@
+class Book{
+    constructor(title,author,pages,isRead){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead
+    }
+   
+}
+
+export default Book;
